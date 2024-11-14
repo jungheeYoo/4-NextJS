fc921d9a6174e72a884b5eb2791eec96011d1753
Joy <jungheeyoo.joy@gmail.com>
2024-11-14 17:43:20 +0900

    2-1. Defining Routes, 새로운 페이지 만드는 방법

---
+++ 1.introduction/app/about-us/company/page.tsx
@@ -0,0 +1,7 @@
+//////////////////////////////////////////////////
+// ✅ 2-1. Defining Routes
+// NextJS 애플리케이션에서 새로운 페이지 만드는 방법
+
+export default function Company() {
+  return <h1>Company!</h1>;
+}
---
+++ 1.introduction/app/about-us/page.tsx
@@ -0,0 +1,9 @@
+//////////////////////////////////////////////////
+// ✅ 2-1. Defining Routes
+// NextJS 애플리케이션에서 새로운 페이지 만드는 방법
+
+export default function AboutUs() {
+  return <h1>About us!</h1>;
+}
+
+// ✨ UI를 꼭 return 해줘야 함
