f01303a51172df309a4b481622b9d16677c3966f
Joy <jungheeyoo.joy@gmail.com>
2024-11-15 18:45:10 +0900

    2-9. Dynamic Routes

---
+++ 2-2.routing/app/(movies)/movies/[id]/page.tsx
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////////
+// ✅ 2-9. Dynamic Routes
+
+export default function MovieDetail({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  return <h1>Movie {id}</h1>;
+}
+
+// 🔶 리액트 라우터에서 하는 것과 같은 것
+// /movies/:id
+
+// 🔶 두 개의 종류의 props를 얻을 수 있다
+// parameters 와 searchParams
+// 파라미터는 URL에서 변수이다
