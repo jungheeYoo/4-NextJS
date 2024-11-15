6b61c99b86ca4d2fb187b24a75964dc0b6df8c63
Joy <jungheeyoo.joy@gmail.com>
2024-11-15 17:53:08 +0900

    2-8. Metadata route groups 사용한 route들 정리, metadata object

---
+++ 2-2.routing/app/about-us/page.tsx
@@ -0,0 +1,48 @@
+// //////////////////////////////////////////////////
+// // ✅ 2-2. Not Found Routes
+// // 네비게이션 바 구현
+// // Not Found Routes
+// // not-found.tsx
+// // usePathname
+
+// import Navigation from '../../components/navigation';
+
+// export default function Page() {
+//   return (
+//     <div>
+//       <Navigation />
+//       <h1>About us!</h1>
+//     </div>
+//   );
+// }
+
+// //////////////////////////////////////////////////
+// // ✅ 2-7. Layouts
+// // 레이아웃 시스템
+// // 레이아웃 시스템에 대해서 배워야 하는 이유는
+// // application을 빌드할 때 재사용하는 요소(element)들이 있기 때문이다.
+
+// export default function Page() {
+//   return (
+//     <div>
+//       <h1>About us!</h1>
+//     </div>
+//   );
+// }
+
+//////////////////////////////////////////////////
+// ✅ 2-8. Metadata
+// route groups을 사용하여 route들을 정리하는 방법
+// metadata object
+
+export const metadata = {
+  title: 'About us',
+};
+
+export default function AboutUs() {
+  return (
+    <div>
+      <h1>About us!</h1>
+    </div>
+  );
+}
